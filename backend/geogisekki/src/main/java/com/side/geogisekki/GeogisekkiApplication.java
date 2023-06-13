ebb46e3212ab2d0018707de29450c0769aa273ba
kdh-92 <kdh920411@gmail.com>
2023-06-13 13:41:23 +0900

    backend start.spring.io init

---
+++ backend/geogisekki/src/main/java/com/side/geogisekki/GeogisekkiApplication.java
@@ -0,0 +1,13 @@
+package com.side.geogisekki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeogisekkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeogisekkiApplication.class, args);
+	}
+
+}
---
+++ backend/geogisekki/src/test/java/com/side/geogisekki/GeogisekkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.geogisekki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeogisekkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
