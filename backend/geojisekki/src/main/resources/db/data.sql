980963183cc2e059830bb5e740e9ece95012f7cf
kdh-92 <kdh920411@gmail.com>
2023-06-23 09:04:04 +0900

    geoji data에 해림, 세윤 데이터 추가

--- backend/geojisekki/src/main/resources/db/data.sql
+++ backend/geojisekki/src/main/resources/db/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO geoji (username, email, birth, password, nickname)
 VALUES ('kdh', 'kdh@email.com', 920411, 'kdh', 'kdh'),
-       ('권동현', 'kwondonghyun@email.com', 920411, '권동현', '권동현');
+       ('권동현', 'kwondonghyun@email.com', 920411, '권동현', '권동현'),
+       ('정해림', 'jhr@email.com', 20000101, 'jhr', '정해림'),
+       ('김세윤', 'ksy@email.com', 20000102, 'ksy', '김세윤');
