018dcb97e51598e696bbb4ab91257f32400a3366
kdh-92 <kdh920411@gmail.com>
2023-06-23 23:46:24 +0900

    spring.jpa.hibernate.ddl-auto 속성을 none으로 변경 후 Geoji & Grade 테이블에 맞는 schema.sql, data.sql 생성
    ### ddl-auto 속성을 update에서 none으로 변경한 이유
    - update의 경우 Entity를 만들게 되면 지정한 PK 값과 다르게 자동 생성됨
    - sql문을 통해 테이블과 데이터를 만드는 상황에서 ddl-auto를 사용할 이유가 없다 판단
    - product에서 ddl-auto를 사용하지 않기 때문에 최종적으로 none으로 변경 후 schema.sql을 통해 테이블 생성

--- backend/geojisekki/src/main/resources/db/data.sql
+++ backend/geojisekki/src/main/resources/db/data.sql
@@ -1,3 +1,6 @@
+INSERT INTO grade (grade_name, grade_description)
+VALUES ('geoji', 'lv 1 geoji');
+
 INSERT INTO geoji (username, email, birth, password, nickname)
 VALUES ('kdh', 'kdh@email.com', 920411, 'kdh', 'kdh'),
        ('권동현', 'kwondonghyun@email.com', 920411, '권동현', '권동현'),
