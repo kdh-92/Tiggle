0ad79a0b43b1d1f6edf245b2fe0635c9f55f1338
Jeong-hae-rim <usemono33@gmail.com>
2023-07-06 13:33:35 +0900

    feat: 설정 파일 관련 추가

---
+++ frontend/tiggle/webpack.config.prod.js
@@ -0,0 +1,19 @@
+const { merge } = require("webpack-merge");
+const path = require('path');
+const webpack = require("webpack");
+const baseConfig = require("./webpack.config");
+
+module.exports = merge(baseConfig, {
+  mode: "production",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_PROD_API_URL),
+    }),
+  ],
+});
