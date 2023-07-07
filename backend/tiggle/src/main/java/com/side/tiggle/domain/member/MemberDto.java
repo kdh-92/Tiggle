4013811ed1b84d7ccdf334a70816f1258dbbd141
ChoiSunwoo <cjd3549@gmail.com>
2023-07-08 01:25:35 +0900

    memberDto 추가

--- backend/tiggle/src/main/java/com/side/tiggle/domain/member/MemberDto.java
+++ backend/tiggle/src/main/java/com/side/tiggle/domain/member/MemberDto.java
@@ -1,10 +1,14 @@
 package com.side.tiggle.domain.member;
 
 import com.side.tiggle.domain.member.model.Member;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class MemberDto {
     long id;
     String email;
