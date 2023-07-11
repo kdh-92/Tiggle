cafe9190cac2c0c8f5607a7f639e9cf31d64d0f2
kdh-92 <kdh920411@gmail.com>
2023-07-11 23:28:27 +0900

    grade api, dto, service, repository 추가

---
+++ backend/tiggle/src/main/java/com/side/tiggle/domain/grade/api/GradeApiController.java
@@ -0,0 +1,42 @@
+package com.side.tiggle.domain.grade.api;
+
+import com.side.tiggle.domain.grade.GradeDto;
+import com.side.tiggle.domain.grade.service.GradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/grade")
+public class GradeApiController {
+
+    private final GradeService gradeService;
+
+    @PostMapping
+    public ResponseEntity<GradeDto> createGrade(@RequestBody GradeDto gradeDto) {
+        return new ResponseEntity<>(gradeService.createGrade(gradeDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GradeDto> getGrade(@PathVariable("id") Long gradeId) {
+        return new ResponseEntity<>(gradeService.getGrade(gradeId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<GradeDto>> getAllGrade() {
+        return new ResponseEntity<>(gradeService.getAllGrade(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GradeDto> updateGrade(@PathVariable("id") Long gradeId,
+                                                @RequestBody GradeDto gradeDto) {
+        return new ResponseEntity<>(gradeService.updateGrade(gradeId, gradeDto), HttpStatus.OK);
+    }
+
+    // delete
+
+}
