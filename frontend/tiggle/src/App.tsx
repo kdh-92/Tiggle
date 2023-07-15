14195f772b85a830d38d2ab5c393cd673abfdcc1
Jeong-hae-rim <usemono33@gmail.com>
2023-07-15 14:54:44 +0900

    feat: ant-design 세팅

--- frontend/tiggle/src/App.tsx
+++ frontend/tiggle/src/App.tsx
@@ -1,3 +1,4 @@
+import { Rate } from 'antd';
 
 const App = () => {
 
@@ -5,6 +6,8 @@ const App = () => {
         <div>
             <h1>{process.env.NODE_ENV}</h1>
             <h1>{process.env.REACT_APP_API_URL}</h1>
+            {/* antd 적용되었는지 확인하기 위한 테스트용 */}
+            <Rate allowHalf defaultValue={2.5} />
         </div>
     )
 }
