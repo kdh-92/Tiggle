ef5915a7ca7c63aed9462d3ded5ede9f94c722fc
Jeong-hae-rim <usemono33@gmail.com>
2023-07-19 23:30:43 +0900

    feat: react-router 세팅 및 main 컴포넌트 생성

--- frontend/tiggle/src/index.tsx
+++ frontend/tiggle/src/index.tsx
@@ -1,19 +1,22 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Router from "./Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./store";
 
 const queryClient = new QueryClient();
-
 const container = document.getElementById("root");
 const root = createRoot(container);
+
 root.render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
-        <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </QueryClientProvider>
   </Provider>
 );
---
+++ frontend/tiggle/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+
+
+const Main = () => {
+  return (
+    <div>
+      Main
+    </div>
+  );
+};
+
+export default Main;
