0da652aa88ca8bbca95d5f342300cf7356812cf2
kdh-92 <kdh920411@gmail.com>
2023-07-26 22:32:18 +0900

    docker 컨테이너 추가 (app & db) - 로컬에 도커 컨테이너 실행해주는 server.sh 생성

---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# backend/tiggle 디렉토리로 이동
+cd backend/tiggle
+
+# ./gradlew bootjar 실행
+./gradlew bootjar
+
+# 실행 중인 tiggle & mysql 컨테이너 제거
+docker-compose down
+
+# tiggle & mysql 백그라운드 실행
+docker-compose up -d --build
+
+# 이미지를 새로 생성하며 기존 unused (dangling) 이미지 삭제 처리
+docker image prune --force
