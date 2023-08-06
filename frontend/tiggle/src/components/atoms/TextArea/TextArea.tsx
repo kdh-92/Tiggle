79b8fa5fd5d2a40609de3f453299b43162b9ea96
MeganMagic <at.fence.3@gmail.com>
2023-08-06 17:02:01 +0900

    feat: textarea 컴포넌트 작성

---
+++ frontend/tiggle/src/components/atoms/TextArea/TextArea.tsx
@@ -0,0 +1,18 @@
+import { InputProps } from "antd";
+import cn from "classnames";
+
+import { StyledTextArea } from "@/styles/components/TextAreaStyle";
+
+interface TextAreaProps extends InputProps {
+  variant: "default" | "filled";
+}
+
+export default function TextArea({
+  variant = "default",
+  className,
+  ...props
+}: TextAreaProps) {
+  return (
+    <StyledTextArea className={cn(className, variant)} rows={3} {...props} />
+  );
+}
