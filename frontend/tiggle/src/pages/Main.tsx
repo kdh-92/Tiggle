5bb8a0fc55921068b2054b274b9e9d2bca6d973e
Jeong-hae-rim <usemono33@gmail.com>
2023-08-15 21:57:30 +0900

    feat: sections 디렉토리 생성 및 ContentSection 컴포넌트 위치 변경

--- frontend/tiggle/src/pages/Main.tsx
+++ frontend/tiggle/src/pages/Main.tsx
@@ -1,12 +1,12 @@
-import Content from "@/components/organisms/Content";
 import Header from "@/components/organisms/Header";
 import Tab from "@/components/organisms/Tab";
+import ContentSection from "@/components/sections/ContentSection/ContentSection";
 
 const Main = () => {
   return (
     <>
       <Header />
-      <Content />
+      <ContentSection />
       <Tab />
     </>
   );
