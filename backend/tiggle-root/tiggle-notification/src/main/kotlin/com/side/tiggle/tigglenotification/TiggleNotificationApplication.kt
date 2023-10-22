f9c429641bed39f84bc06003be8b3778ec78bd60
최선우 <cjd3549@gmail.com>
2023-10-22 17:10:37 +0900

    Notification Server 셋업

---
+++ backend/tiggle-root/tiggle-notification/src/main/kotlin/com/side/tiggle/tigglenotification/TiggleNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.side.tiggle.tigglenotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TiggleNotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<TiggleNotificationApplication>(*args)
+}
