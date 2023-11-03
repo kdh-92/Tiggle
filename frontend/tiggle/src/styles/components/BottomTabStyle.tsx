00b852df412f4cdfc71905acb3a7982c0d5ccd47
jeong-hae-rim <usemono33@gmail.com>
2023-11-04 00:33:04 +0900

    feat: Tab 컴포넌트 이름BottomTab으로 변경 및 스토리북 컴포넌트 구현, TabButton 컴포넌트 삭제 및 간단한 동작 추가

--- frontend/tiggle/src/styles/components/BottomTabStyle.tsx
+++ frontend/tiggle/src/styles/components/BottomTabStyle.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "antd/es/layout/layout";
 import styled from "styled-components";
 
-export const TabStyle = styled(Footer)`
+export const BottomTabStyle = styled(Footer)`
   position: sticky;
   bottom: 0;
   z-index: 1;
@@ -10,8 +10,7 @@ export const TabStyle = styled(Footer)`
   padding: 0;
   display: grid;
   grid-auto-flow: column;
-  background: ${({ theme: { color } }) => color.white.value};
-  border-top: 1px solid ${({ theme: { color } }) => color.bluishGray[200].value};
+  background: ${({ theme: { color } }) => color.bluishGray[50].value};
 
   .tab-button {
     display: flex;
@@ -21,13 +20,13 @@ export const TabStyle = styled(Footer)`
     padding: 0 0 4px 0;
     font-size: 9px;
     gap: 4px;
-    background: ${({ theme: { color } }) => color.white.value};
+    background: ${({ theme: { color } }) => color.bluishGray[50].value};
     justify-content: center;
-    color: ${({ theme: { color } }) => color.bluishGray[400].value};
+    color: ${({ theme: { color } }) => color.bluishGray[500].value};
   }
 
   .focus {
-    color: ${({ theme: { color } }) => color.black.value};
+    color: ${({ theme: { color } }) => color.blue[600].value};
   }
 
   ${({ theme }) => theme.mq.desktop} {
