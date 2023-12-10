21c6ad7a21252ea3eca7cc128ee5774cc907bac4
jeong-hae-rim <usemono33@gmail.com>
2023-12-11 02:30:17 +0900

    feat: Mypage user setting 구현

--- frontend/tiggle/src/pages/MyPage.tsx
+++ frontend/tiggle/src/pages/MyPage.tsx
@@ -1,3 +1,5 @@
+import { ChevronRight } from "react-feather";
+
 import { useQuery } from "@tanstack/react-query";
 import { Avatar } from "antd";
 import styled from "styled-components";
@@ -57,6 +59,43 @@ export const MypageStyle = styled.div`
       ${({ theme }) => expandTypography(theme.typography.body.medium.bold)}
     }
   }
+
+  .user-transaction {
+    margin-bottom: 80px;
+    .transaction-title {
+      color: ${({ theme: { color } }) => color.bluishGray[600].value};
+      ${({ theme }) => expandTypography(theme.typography.body.large.bold)}
+    }
+  }
+
+  .user-setting {
+    .setting-title {
+      color: ${({ theme: { color } }) => color.bluishGray[600].value};
+      ${({ theme }) => expandTypography(theme.typography.body.large.bold)}
+      margin-bottom: 12px;
+    }
+    .setting-cells {
+      width: 480px;
+      background: ${({ theme }) => theme.color.white.value};
+      border-radius: 16px;
+      .setting-cell {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 72px;
+        padding: 24px 28px;
+        color: ${({ theme: { color } }) => color.bluishGray[700].value};
+        border-bottom: 1px solid
+          ${({ theme: { color } }) => color.bluishGray[100].value};
+        &:last-child {
+          border-bottom: none;
+        }
+        > svg {
+          color: ${({ theme: { color } }) => color.bluishGray[400].value};
+        }
+      }
+    }
+  }
 `;
 
 const MyPage = () => {
@@ -91,10 +130,21 @@ const MyPage = () => {
               <button className="profile-modify-button">프로필 수정</button>
             </div>
             <div className="user-transaction">
-              <p>내 거래 기록</p>
+              <p className="transaction-title">내 거래 기록</p>
             </div>
             <div className="user-setting">
-              <p>설정</p>
+              <p className="setting-title">설정</p>
+              <div className="setting-cells">
+                <div className="setting-cell">
+                  <span>자산 항목 관리</span> <ChevronRight />
+                </div>
+                <div className="setting-cell">
+                  <span>지출 카테고리 관리</span> <ChevronRight />
+                </div>
+                <div className="setting-cell">
+                  <span>수입 카테고리 관리</span> <ChevronRight />
+                </div>
+              </div>
             </div>
             <button className="logout-button">로그아웃</button>
           </>
