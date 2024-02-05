6c7e55c63d0e5388bf3dfb739f4d62934fcd3ad6
ChoiSunwoo <cjd3549@gmail.com>
2024-02-06 00:34:07 +0900

    전체를 코틀린으로 전환 완료

--- backend/tiggle-root/tiggle/src/main/java/com/side/tiggle/global/exception/NotAuthorizedException.java
+++
@@ -1,3 +0,0 @@
-package com.side.tiggle.global.exception;
-
-public class NotAuthorizedException extends CustomException { }
---
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/TiggleApplication.kt
@@ -0,0 +1,10 @@
+package com.side.tiggle
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TiggleApplication
+fun main(args: Array<String>) {
+    SpringApplication.run(TiggleApplication::class.java, *args)
+}
