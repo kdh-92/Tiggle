d18c4e541dea7fca5b92c8f094af41b0219233bb
ChoiSunwoo <cjd3549@gmail.com>
2024-02-12 20:41:19 +0900

    categoryRespDto에 type 추가

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/resp/CategoryRespDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/resp/CategoryRespDto.kt
@@ -1,18 +1,21 @@
 package com.side.tiggle.domain.category.dto.resp
 
 import com.side.tiggle.domain.category.model.Category
+import com.side.tiggle.domain.category.model.CategoryType
 
 data class CategoryRespDto(
     val id: Long,
     val name: String,
-    val defaults: Boolean
+    val defaults: Boolean,
+    val type: CategoryType
 ) {
     companion object {
         fun fromEntity(category: Category): CategoryRespDto {
             return CategoryRespDto(
                 id = category.id!!,
                 name = category.name,
-                defaults = category.defaults
+                defaults = category.defaults,
+                type = category.type
             )
         }
     }
