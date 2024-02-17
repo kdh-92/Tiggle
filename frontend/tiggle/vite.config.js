bdfb1cbc6f293c4cfee8e34877eebe7fb621b8f8
MeganMagic <at.fence.3@gmail.com>
2024-02-17 14:41:16 +0900

    feat: vite setup

---
+++ frontend/tiggle/vite.config.js
@@ -0,0 +1,17 @@
+import * as path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
