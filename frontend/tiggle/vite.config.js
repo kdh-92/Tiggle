1559b491cb79a2d73404d992377c1449648bb1ca
MeganMagic <at.fence.3@gmail.com>
2024-03-11 23:09:03 +0900

    fix: eslint 오류 수정

--- frontend/tiggle/vite.config.js
+++ frontend/tiggle/vite.config.js
@@ -2,11 +2,11 @@ import * as path from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
 
-
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), eslint()],
   server: {
     port: 3000,
   },
@@ -18,6 +18,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ['./vitest.setup.ts']
-  }
+    setupFiles: ["./vitest.setup.ts"],
+  },
 });
