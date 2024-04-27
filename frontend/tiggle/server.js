48379a7f7f7a5898e69dfe24ca5a3ad18ad06712
ChoiSunwoo <cjd3549@gmail.com>
2024-04-27 15:47:40 +0900

    fe 쪽 라우터 이슈를 해결하기 위해 동적 라우팅용 서버 코드 추가

---
+++ frontend/tiggle/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import * as path from "node:path";
+
+const app = express();
+const port = 3001;
+
+ViteExpress.config({
+    mode: "production",
+});
+
+ViteExpress.listen(app, port, () => console.log(`ViteExpress server open to ${port}`))
