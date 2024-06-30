02b44c3a36cffad31806cc5049944390ae2bc938
jeong-hae-rim <usemono33@gmail.com>
2024-06-30 14:57:54 +0900

    feat: 메인페이지 알람 모달 css  조정

--- frontend/tiggle/src/pages/MainPage/index.tsx
+++ frontend/tiggle/src/pages/MainPage/index.tsx
@@ -1,4 +1,6 @@
 // import BottomTab from "@/components/molecules/BottomTab/BottomTab";
+import { useSelector } from "react-redux";
+
 import { useQuery } from "@tanstack/react-query";
 
 import BottomTab from "@/components/molecules/BottomTab/BottomTab";
@@ -7,16 +9,20 @@ import Banner from "@/pages/MainPage/Banner/Banner";
 import { MainPageStyle } from "@/pages/MainPage/MainPageStyle";
 import TransactionCells from "@/pages/MainPage/TransactionCell/TransactionCells";
 import { transactionKeys } from "@/query/queryKeys";
+import { RootState } from "@/store";
 
 const MainPage = () => {
   const { data } = useQuery({
     queryKey: transactionKeys.lists(),
     queryFn: () => TransactionApiControllerService.getAllTransaction(),
   });
+  const isModalOpen = useSelector(
+    (state: RootState) => state.notificationModal.isOpen,
+  );
 
   return (
     <>
-      <MainPageStyle>
+      <MainPageStyle open={isModalOpen}>
         <Banner />
         <TransactionCells data={data!} />
         <BottomTab />
