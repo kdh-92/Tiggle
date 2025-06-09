ea4c45a08957e3ca2c944c684e375c81414c4d99
W.llama <106076670+W-llama@users.noreply.github.com>
2025-06-09 09:58:14 +0900

    삭제된 카테고리 참조로 인한 에러 해결 (CategoryRespDto.fromEntity()에서 예외 처리 추가, 삭제된 카테고리는 "삭제된 카테고리"로 표시,null 카테고리는 "카테고리 없음"으로 표시)

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/resp/CategoryRespDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/resp/CategoryRespDto.kt
@@ -3,17 +3,31 @@ package com.side.tiggle.domain.category.dto.resp
 import com.side.tiggle.domain.category.model.Category
 
 data class CategoryRespDto(
-    val id: Long,
+    val id: Long?,
     val name: String,
     val defaults: Boolean
 ) {
     companion object {
-        fun fromEntity(category: Category): CategoryRespDto {
-            return CategoryRespDto(
-                id = category.id!!,
-                name = category.name,
-                defaults = category.defaults,
-            )
+        fun fromEntity(category: Category?): CategoryRespDto {
+            return try {
+                category?.let {
+                    CategoryRespDto(
+                        id = it.id!!,
+                        name = it.name,
+                        defaults = it.defaults
+                    )
+                } ?: CategoryRespDto(
+                    id = null,
+                    name = "카테고리 없음",
+                    defaults = false
+                )
+            } catch (e: Exception) {
+                CategoryRespDto(
+                    id = null,
+                    name = "삭제된 카테고리",
+                    defaults = false
+                )
+            }
         }
     }
 }
