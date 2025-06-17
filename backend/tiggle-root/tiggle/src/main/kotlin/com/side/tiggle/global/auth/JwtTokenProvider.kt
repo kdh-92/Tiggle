16fec14dfbe7c90e78b1fd1e6e0e52b17976c0d9
Atriel <fortuny1999@gmail.com>
2025-06-18 03:08:09 +0900

    📦 Chore: (Java 21) jjwt 버전 업그레이드 (0.11.5 -> 0.12.6) Spring boot 3.5.0과 호환맞는 release버전, 변경에따른 jwt Token 생성 방식 수정

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/global/auth/JwtTokenProvider.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/global/auth/JwtTokenProvider.kt
@@ -34,14 +34,13 @@ class JwtTokenProvider(
     ): String {
         val issued = Date.from(LocalDateTime.now().atZone(zone).toInstant())
         val expiry = Date.from(LocalDateTime.now().plusSeconds(validFor).atZone(zone).toInstant())
-        val claims = Jwts.claims().setSubject(memberId.toString())
-        claims.put("roles", roles)
 
         return Jwts.builder()
-            .setIssuer("tiggle")
-            .setClaims(claims)
-            .setIssuedAt(issued)
-            .setExpiration(expiry)
+            .issuer("tiggle")
+            .subject(memberId.toString())
+            .claim("roles", roles)
+            .issuedAt(issued)
+            .expiration(expiry)
             .signWith(secretKey)
             .compact()
     }
@@ -73,19 +72,24 @@ class JwtTokenProvider(
     }
 
     fun isTokenValid(token: String): Boolean {
-        val expiry: Date = extractClaims(token).expiration
-        return expiry.after(Date())
+        return try {
+            val expiry: Date = extractClaims(token).expiration
+            expiry.after(Date())
+        } catch (e: Exception) {
+            println("토큰 검증 실패: ${e.message}")
+            false
+        }
     }
 
     private fun extractClaims(
         jwtToken: String
     ): Claims {
         try {
-            return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
+            return Jwts.parser()
+                .verifyWith(secretKey)
                 .build()
-                .parseClaimsJws(jwtToken)
-                .body
+                .parseSignedClaims(jwtToken)
+                .payload
         } catch (e: JwtException) {
             throw IllegalStateException()
         }
