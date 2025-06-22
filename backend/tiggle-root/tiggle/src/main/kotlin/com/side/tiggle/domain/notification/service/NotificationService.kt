392398b016b61113fef33101f7e2926844abcec4
Atriel <fortuny1999@gmail.com>
2025-06-23 05:18:56 +0900

    Refactor: #192 - notification: Notification 도메인 Dto 분리 구조 개선

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/notification/service/NotificationService.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/notification/service/NotificationService.kt
@@ -2,17 +2,13 @@ package com.side.tiggle.domain.notification.service
 
 import com.side.tiggle.domain.comment.dto.resp.CommentRespDto
 import com.side.tiggle.domain.comment.model.Comment
-import com.side.tiggle.domain.comment.service.CommentService
-import com.side.tiggle.domain.member.dto.MemberDto
 import com.side.tiggle.domain.member.model.Member
 import com.side.tiggle.domain.member.service.MemberService
 import com.side.tiggle.domain.notification.NotificationProducer
-import com.side.tiggle.domain.notification.dto.NotificationDto
+import com.side.tiggle.domain.notification.dto.resp.NotificationRespDto
 import com.side.tiggle.domain.notification.dto.NotificationProduceDto
-import com.side.tiggle.domain.notification.model.Notification
 import com.side.tiggle.domain.notification.repository.NotificationRepository
 import com.side.tiggle.domain.transaction.model.Transaction
-import com.side.tiggle.domain.transaction.service.TransactionService
 import com.side.tiggle.global.exception.NotFoundException
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -24,17 +20,17 @@ class NotificationService(
     private val memberService: MemberService,
 ) {
 
-    fun getAllByMemberId(memberId: Long): List<NotificationDto> {
+    fun getAllByMemberId(memberId: Long): List<NotificationRespDto> {
         val member = memberService.getMemberOrThrow(memberId)
         val notiList = notificationRepository.findAllByReceiver(member)
         return notiList.map {
             val sender = if (it.sender != null) {
-                MemberDto.fromEntity(it.sender!!)
+                MemberRespDto.fromEntity(it.sender!!)
             } else {
                 null
             }
-            val receiver = MemberDto.fromEntity(it.receiver!!)
-            NotificationDto.fromEntity(it)
+            val receiver = MemberRespDto.fromEntity(it.receiver!!)
+            NotificationRespDto.fromEntity(it)
                 .apply {
                     this.sender = sender
                     this.receiver = receiver
