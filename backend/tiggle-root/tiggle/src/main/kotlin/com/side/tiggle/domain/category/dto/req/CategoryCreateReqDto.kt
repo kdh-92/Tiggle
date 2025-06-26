e394cd50c3409c6f31125d42075bf7124b33f9ce
Atriel <fortuny1999@gmail.com>
2025-06-27 08:36:16 +0900

    Feat: #189: category - 컨트롤러, request Dto에 검증로직 추가

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/req/CategoryCreateReqDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/category/dto/req/CategoryCreateReqDto.kt
@@ -2,9 +2,13 @@ package com.side.tiggle.domain.category.dto.req
 
 import com.side.tiggle.domain.category.model.Category
 import com.side.tiggle.domain.member.model.Member
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class CategoryCreateReqDto (
-    val name : String
+    @field:NotBlank(message = "카테고리명은 필수입니다")
+    @field:Size(max = 100, message = "카테고리명은 100자 이하여야 합니다")
+    val name: String
 ) {
     fun toEntity(member: Member): Category {
         return Category(
