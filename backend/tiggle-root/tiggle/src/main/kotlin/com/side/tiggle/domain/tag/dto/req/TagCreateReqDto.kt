53f7289b7246695fd7683625b0ca5f81dc225ff1
Atriel <fortuny1999@gmail.com>
2025-06-26 09:02:20 +0900

    Feat: #189: Tag - 컨트롤러, request Dto에 검증로직 추가

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/tag/dto/req/TagCreateReqDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/tag/dto/req/TagCreateReqDto.kt
@@ -1,5 +1,10 @@
 package com.side.tiggle.domain.tag.dto.req
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class TagCreateReqDto(
+    @field:NotBlank(message = "태그명은 필수입니다")
+    @field:Size(max = 50, message = "태그명은 50자 이하여야 합니다")
     val name: String
 )
