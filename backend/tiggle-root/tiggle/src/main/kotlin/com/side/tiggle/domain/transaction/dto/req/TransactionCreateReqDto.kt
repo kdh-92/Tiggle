08f5fe240f38d99436eebbc95ab1c5f467ec0b42
Atriel <fortuny1999@gmail.com>
2025-06-28 08:54:57 +0900

    Fix: #189: transaction - import문 정리

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/req/TransactionCreateReqDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/req/TransactionCreateReqDto.kt
@@ -6,7 +6,6 @@ import com.side.tiggle.domain.transaction.model.Transaction
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.PastOrPresent
 import jakarta.validation.constraints.PositiveOrZero
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/req/TransactionUpdateReqDto.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/req/TransactionUpdateReqDto.kt
@@ -3,7 +3,6 @@ package com.side.tiggle.domain.transaction.dto.req
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.PastOrPresent
 import jakarta.validation.constraints.PositiveOrZero
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
