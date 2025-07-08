99e3d82116086efb019a970e2ab58a944840c3b0
Atriel <fortuny1999@gmail.com>
2025-07-09 07:32:54 +0900

    Refactor: #206: front: 메인페이지 받아오는 응답객체 수정

--- frontend/tiggle/src/pages/MainPage/TransactionCell/TransactionCells.tsx
+++ frontend/tiggle/src/pages/MainPage/TransactionCell/TransactionCells.tsx
@@ -1,29 +1,26 @@
 import Masonry from "react-masonry-css";
 
-import { useQuery } from "@tanstack/react-query";
-
-import { TransactionApiControllerService } from "@/generated";
-import { TransactionRespDto } from "@/generated/models/TransactionRespDto";
+import { TransactionDtoWithCount } from "@/generated/models/TransactionDtoWithCount";
 import { TransactionCellsStyle } from "@/pages/MainPage/TransactionCell/TransactionCellsStyle";
-import { transactionKeys } from "@/query/queryKeys";
 
 import TransactionCell from "./TransactionCell";
 import TransactionCellSkeleton from "./TransactionCellSkeleton";
 
 interface TransactionCellsProps {
-  data: TransactionRespDto[];
+  data: TransactionDtoWithCount[] | undefined;
+  isLoading?: boolean;
+  isError?: boolean;
 }
 
-export default function TransactionCells({ data }: TransactionCellsProps) {
+export default function TransactionCells({
+  data,
+  isLoading = false,
+  isError = false,
+}: TransactionCellsProps) {
   const breakpointColumnsObj = {
     default: 2,
     767: 1,
   };
-
-  const { isError, isLoading } = useQuery({
-    queryKey: transactionKeys.lists(),
-    queryFn: () => TransactionApiControllerService.getAllTransaction(),
-  });
 
   return (
     <>
@@ -37,8 +34,10 @@ export default function TransactionCells({ data }: TransactionCellsProps) {
             [...Array(4)].map((_, i) => <TransactionCellSkeleton key={i} />)}
           {!isLoading &&
             !isError &&
-            data?.transactions?.map(el => {
-              return <TransactionCell key={el.id} {...el} />;
+            data?.map(transaction => {
+              return (
+                <TransactionCell key={transaction.dto.id} {...transaction} />
+              );
             })}
           {isError && <p>An error occurred.</p>}
         </Masonry>
