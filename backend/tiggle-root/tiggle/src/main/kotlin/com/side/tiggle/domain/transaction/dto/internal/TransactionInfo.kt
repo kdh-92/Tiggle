6068df3bddc9027da7ab8075347587780c18c88d
Atriel <fortuny1999@gmail.com>
2025-07-12 06:42:12 +0900

    Fix: #206: transaction - TransactionInfo에서 불필요한 non-null assertion 제거

--- backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/internal/TransactionInfo.kt
+++ backend/tiggle-root/tiggle/src/main/kotlin/com/side/tiggle/domain/transaction/dto/internal/TransactionInfo.kt
@@ -18,7 +18,7 @@ data class TransactionInfo(
         fun fromEntity(transaction: Transaction): TransactionInfo {
             return TransactionInfo(
                 id = transaction.id!!,
-                memberId = transaction.member.id!!,
+                memberId = transaction.member.id,
                 categoryId = transaction.category.id!!,
                 imageUrl = transaction.imageUrl,
                 amount = transaction.amount,
